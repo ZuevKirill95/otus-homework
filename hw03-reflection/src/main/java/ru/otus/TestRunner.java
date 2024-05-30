ce531db5bc20b66a0ae1fcdc57b8e4a61967bfc7
Kirill Zuev <k.zuev@vk.team>
2024-05-30 21:25:36 +0300

    Исправлено замечание

--- hw03-reflection/src/main/java/ru/otus/TestRunner.java
+++ hw03-reflection/src/main/java/ru/otus/TestRunner.java
@@ -1,7 +1,6 @@
 package ru.otus;
 
 import lombok.experimental.UtilityClass;
-import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,10 +9,8 @@ import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.List;
 
 @UtilityClass
@@ -30,28 +27,32 @@ public final class TestRunner {
         for (Method method : testMethods) {
             LOGGER.info("_________Test_Begin_________");
             try {
-                T prevObject = invokeMethods(tClass, Before.class, null);
                 object = tClass.getConstructor().newInstance();
-                moveFieldValuesFromPrevObjectToNewObject(object, prevObject);
+
+                invokeMethods(object, Before.class);
 
                 MethodUtils.invokeMethod(object, true, method.getName());
 
                 LOGGER.info("Test {} passed", method.getName());
 
-                invokeMethods(tClass, After.class, object);
+                invokeMethods(object, After.class);
                 LOGGER.info("_________Test_end_________\n");
             } catch (InvocationTargetException e) {
                 if (e.getTargetException() instanceof AssertionError assertionError) {
                     LOGGER.warn(assertionError.getMessage());
                     failedTestsCount++;
 
-                    invokeMethods(tClass, After.class, object);
+                    if (object != null){
+                        invokeMethods(object, After.class);
+                    }
                     LOGGER.info("_________Test_end_________\n");
                 } else {
                     LOGGER.error("Test {} выбросил исключение", method.getName(), e);
                     failedTestsCount++;
 
-                    invokeMethods(tClass, After.class, object);
+                    if (object != null){
+                        invokeMethods(object, After.class);
+                    }
                     LOGGER.info("_________Test_end_________\n");
                 }
             } catch (NoSuchMethodException | IllegalAccessException | InstantiationException e) {
@@ -70,42 +71,16 @@ public final class TestRunner {
                 testCount, successTestsCount, failedTestsCount);
     }
 
-    private static <T> T invokeMethods(Class<T> tClass, Class<? extends Annotation> annotationClass, T prevObject) {
+    private static void invokeMethods(Object object, Class<? extends Annotation> annotationClass) {
         List<Method> methodsListWithAnnotation =
-                MethodUtils.getMethodsListWithAnnotation(tClass, annotationClass, false, true);
+                MethodUtils.getMethodsListWithAnnotation(object.getClass(), annotationClass, false, true);
 
         for (Method method : methodsListWithAnnotation) {
             try {
-                T object = tClass.getConstructor().newInstance();
-                moveFieldValuesFromPrevObjectToNewObject(object, prevObject);
-
                 MethodUtils.invokeMethod(object, true, method.getName());
 
-                prevObject = object;
             } catch (Exception e) {
                 throw new RuntimeException(e);
-            }
-        }
-
-        return prevObject;
-    }
-
-    private static <T> void moveFieldValuesFromPrevObjectToNewObject(T object, T prevObject)
-            throws IllegalAccessException {
-        Field[] declaredFields = object.getClass().getDeclaredFields();
-
-        for (Field field : declaredFields) {
-            if (Modifier.isStatic(field.getModifiers())) {
-                continue;
-            }
-
-            if (prevObject != null) {
-                for (Field prevField : prevObject.getClass().getDeclaredFields()) {
-                    if (prevField.getName().equals(field.getName())) {
-                        Object prevValue = FieldUtils.readField(prevObject, prevField.getName(), true);
-                        FieldUtils.writeField(object, field.getName(), prevValue, true);
-                    }
-                }
             }
         }
     }
