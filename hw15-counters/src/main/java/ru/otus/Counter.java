e7bb8e95efd241f3f23d8ce27e9decd47b6b0dd6
Kirill Zuev <k.zuev@vk.team>
2024-08-28 07:45:47 +0300

    Executors. ДЗ

--- hw15-counters/src/main/java/ru/otus/Counter.java
+++ hw15-counters/src/main/java/ru/otus/Counter.java
@@ -7,7 +7,7 @@ public class Counter {
     private static final Logger logger = LoggerFactory.getLogger(Counter.class);
 
     private static final Object lock = new Object();
-    private static volatile String runningThread = "t1";
+    private static String runningThread = "t1";
 
     public static void main(String[] args) {
         Thread thread1 = new Thread(() -> printNumbers("t2"));
@@ -19,29 +19,27 @@ public class Counter {
         thread2.start();
     }
 
-    private static void printNumbers(String anotherThreadName) {
-        synchronized (lock) {
-            try {
-                for (int i = 1; i <= 10; i++) {
-                    while (!runningThread.equals(Thread.currentThread().getName())) {
-                        lock.wait();
-                    }
-                    logger.info("count {}", i);
-                    runningThread = anotherThreadName;
-                    lock.notifyAll();
+    private static synchronized void printNumbers(String anotherThreadName) {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                while (!runningThread.equals(Thread.currentThread().getName())) {
+                    lock.wait();
                 }
-
-                for (int i = 9; i > 0; i--) {
-                    while (!runningThread.equals(Thread.currentThread().getName())) {
-                        lock.wait();
-                    }
-                    logger.info("count {}", i);
-                    runningThread = anotherThreadName;
-                    lock.notifyAll();
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                logger.info("count {}", i);
+                runningThread = anotherThreadName;
+                lock.notifyAll();
             }
+
+            for (int i = 9; i > 0; i--) {
+                while (!runningThread.equals(Thread.currentThread().getName())) {
+                    lock.wait();
+                }
+                logger.info("count {}", i);
+                runningThread = anotherThreadName;
+                lock.notifyAll();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
